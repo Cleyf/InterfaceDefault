c749ae615fbd96e81934f643b0680bdac0ea0a3b
Cleyfson Angelo <cleyfson12@outlook.com>
2024-02-20 13:56:47 -0300

    Interface metodos default

---
+++ InterfaceDefault/src/Service/UsaInterestService.java
@@ -0,0 +1,15 @@
+package Service;
+
+public class UsaInterestService implements InterestService {
+
+    private double interestRate;
+
+    public UsaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+}
